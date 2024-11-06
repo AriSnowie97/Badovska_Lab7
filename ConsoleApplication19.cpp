74f67332e5c8097ea69132f774215897f14e6fd2
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-11-06 08:23:18 +0200

    Add files via upload

---
+++ ConsoleApplication19.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int size;
+	cout << "Enter numbers: ";
+	cin >> size;
+	int* mass = new int[size];
+	cout << "Enter elements in array: ";
+	for (int i = 0; i < size; ++i) {
+		cin >> mass[i];
+	}
+	bool isSymmetrychno = true;
+	for (int i = 0; i < size / 2; ++i) {
+		if (mass[i] != mass[size - 1 - i]) {
+			isSymmetrychno = false;
+			break;
+		}
+	}
+	if (isSymmetrychno) {
+		cout << "Array is symmetric" << endl;
+	}
+	else {
+		cout << "Array isn't symmetric" << endl;
+	}
+	delete[] mass;
+	return 0;
+}
